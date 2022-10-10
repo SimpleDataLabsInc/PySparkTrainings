8669dba4c0766755b78181c4c0973dfad883e388
anya.bida1@gmail.com <anya.bida1@gmail.com>
2022-10-10 19:55:53 +0000

    add a column for the orderBy Gem

--- pipelines/ExploreTCPH/code/exploretcph/graph/OrderBy.py
+++ pipelines/ExploreTCPH/code/exploretcph/graph/OrderBy.py
@@ -5,4 +5,4 @@ from exploretcph.config.ConfigStore import *
 from exploretcph.udfs.UDFs import *
 
 def OrderBy(spark: SparkSession, in0: DataFrame) -> DataFrame:
-    return in0.orderBy(col("L_RETURNFLAG").asc(), col("L_LINESTATUS").asc())
+    return in0.orderBy(col("L_RETURNFLAG").asc(), col("L_LINESTATUS").asc(), col("SUM_QTY").asc())
